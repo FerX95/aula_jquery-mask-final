3c409d6a477b5a3b7c46699022e487a6a137574d
Fernando Silva <fernando.jmelo39@gmail.com>
2024-05-08 08:22:24 -0300

    Add files via upload

---
+++ js/action.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.i_data').mask('00/00/0000');
+    $('.i_hora').mask('00:00');
+    $('.i_cep').mask('00000-000');
+    $('.i_telefone').mask('(00)00000-0000');
+    $('.i_rg').mask('00.000.000-0');
+    $('.i_cpf').mask('000.000.000-00');
+});
